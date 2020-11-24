e5a286afbbf9f8925ee260e75cfdf52d3aa63a43
Merissa Acosta <merissa.acosta@gmail.com>
2020-11-24 16:44:02 -0600

    feat(MorningPastriesPage): add new morning pastries page

---
+++ src/pages/morning-pastries.tsx
@@ -0,0 +1,13 @@
+import React, { useContext, useRef } from 'react';
+import Layout from '../components/layout';
+
+const MorningPastriesPage = () => {
+
+  return(
+    <Layout>
+      <div>hi</div>
+    </Layout>
+  );
+}
+
+export default MorningPastriesPage;
