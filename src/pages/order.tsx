89b8468efaea3af922d4348b81b8c4127f3afbc5
Merissa Acosta <merissa.acosta@gmail.com>
2021-01-03 17:52:54 -0600

    cleanup

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -148,47 +148,30 @@ const OrderPage = ({data}: OrderProps) => {
          <Tabs activeTabId={activeTabId} tabsInfo={TABS_DATA} onPress={handleTabPress} />
        </>
       )}
-        {userStatus === 'SignedOut' ? (
-          <button className="snipcart-customer-signin">
-            <span>Login</span>
-          </button>
-        ) : (
-          <button className="snipcart-customer-signin">
-            <span>My account</span>
-          </button>
-        )}
-        <button onClick={addItemToCart}>hi</button>
-        <button className="snipcart-checkout">
-          Click here to checkout
-        </button>
-        <button
-          hidden
-          ref={testingRef}
-          className="snipcart-add-item"
-          data-item-id="silver-stacking-ring"
-          data-item-price="19.99"
-          data-item-url="/order"
-          data-item-name="Silver Stacking Ring"
-          data-item-custom1-name="Size"
-          data-item-custom1-options="6|6.5|7|7.5|8|8.5|9"
-          data-item-custom1-value="7"
-        >
-          Add to cart
-        </button>
-      <div><button onClick={removeAllItemsFromCart}>remove all items from cart</button></div>
-
-      <button
-        className="snipcart-add-item"
-        data-item-id="pickup-from-store"
-        data-item-price="0.00"
-        data-item-url="/order"
-        data-item-name="Pickup from Store"
-        data-item-custom1-name="Time"
-        data-item-custom1-options="9am - 10am|10am - 11am"
-        data-item-custom1-value="10am - 11am"
-      >
-        Add Pickup
-      </button>
+        {/*{userStatus === 'SignedOut' ? (*/}
+        {/*  <button className="snipcart-customer-signin">*/}
+        {/*    <span>Login</span>*/}
+        {/*  </button>*/}
+        {/*) : (*/}
+        {/*  <button className="snipcart-customer-signin">*/}
+        {/*    <span>My account</span>*/}
+        {/*  </button>*/}
+        {/*)}*/}
+        {/*<button*/}
+        {/*  hidden*/}
+        {/*  ref={testingRef}*/}
+        {/*  className="snipcart-add-item"*/}
+        {/*  data-item-id="silver-stacking-ring"*/}
+        {/*  data-item-price="19.99"*/}
+        {/*  data-item-url="/order"*/}
+        {/*  data-item-name="Silver Stacking Ring"*/}
+        {/*  data-item-custom1-name="Size"*/}
+        {/*  data-item-custom1-options="6|6.5|7|7.5|8|8.5|9"*/}
+        {/*  data-item-custom1-value="7"*/}
+        {/*>*/}
+        {/*  Add to cart*/}
+        {/*</button>*/}
+      {/*<div><button onClick={removeAllItemsFromCart}>remove all items from cart</button></div>*/}
       <button onClick={handleOnClick}>test!!!</button>
         <button
           style={{display: 'none'}}
