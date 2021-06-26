4064858b7bd1b37625ecbfb4b89dd37f92af2f35
Merissa Acosta <merissa.acosta@gmail.com>
2021-06-26 15:27:51 -0500

    fix(PartyGiftsSectionTabs): remove unused imports

--- src/components/PartyGiftsSectionTabs.tsx
+++ src/components/PartyGiftsSectionTabs.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import { PartyAndGiftsContent } from '../sharedTypes';
 import { Tabs } from '../design-system';
 import ReactMarkdown from 'react-markdown';
